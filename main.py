903aac41d38d91b8902c8de3be73723316afba22
Sachin Malaji <sachinmalaji677@gmail.com>
2026-06-27 22:37:17 +0530

    fastapiday2

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 #FastAPI 
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# def home():
+#     return {"message": "Hello Sachin"}
+# @app.get("/about")
+# def about():
+#     return {"message": "Learning FastAPI"}
+
+# @app.get("/student/{id}")
+# def student(id: int):
+#     return {"student_id": id}
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Hello Sachin"}
-@app.get("/about")
-def about():
-    return {"message": "Learning FastAPI"}
 
-@app.get("/student/{id}")
-def student(id: int):
-    return {"student_id": id}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
