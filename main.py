e92934ea9f940d85581626079f271b3795546a0a
Sachin Malaji <sachinmalaji677@gmail.com>
2026-07-08 08:46:59 +0530

    day 15-PoatgreSQL connected to FastAPI

--- main.py
+++ main.py
@@ -29,24 +29,77 @@
 #     return {"message": "Here are your posts"}
 
 
+# from fastapi import FastAPI
+# from sqlalchemy import create_engine, Column, Integer, String
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# Database setup
+# DATABASE_URL = "sqlite:///./tasks.db"
+# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+
+# # Task Model (Table)
+# class Task(Base):
+#     __tablename__ = "tasks"
+#     id = Column(Integer, primary_key=True, index=True)
+#     title = Column(String, index=True)
+
+# # Create table
+# Base.metadata.create_all(bind=engine)
+
+# app = FastAPI()
+
+# @app.post("/tasks")
+# def create_task(title: str):
+#     db = SessionLocal()
+#     task = Task(title=title)
+#     db.add(task)
+#     db.commit()
+#     db.refresh(task)
+#     db.close()
+#     return {"id": task.id, "title": task.title}
+
+# @app.get("/tasks")
+# def get_tasks():
+#     db = SessionLocal()
+#     tasks = db.query(Task).all()
+#     db.close()
+#     return {"tasks": tasks}
+
+# @app.delete("/tasks/{task_id}")
+# def delete_task(task_id: int):
+#     db = SessionLocal()
+#     task = db.query(Task).filter(Task.id == task_id).first()
+#     if task:
+#         db.delete(task)
+#         db.commit()
+#         db.close()
+#         return {"message": "Deleted!"}
+#     db.close()
+#     return {"error": "Not found!"}
+
+
+
+
 from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Database setup
-DATABASE_URL = "sqlite:///./tasks.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# PostgreSQL connection
+DATABASE_URL = "postgresql://postgres:sde2@localhost:5432/taskdb"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Task Model (Table)
 class Task(Base):
     __tablename__ = "tasks"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
 
-# Create table
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
